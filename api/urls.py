d010ff1b6978b9f55fa481a1a48caba747c18917
Laurence Zanotti <lau_zanotti@hotmail.com>
2022-10-19 09:15:40 -0300

    Rota GET /api/jogos retorna JSON com lista de jogos

--- api/urls.py
+++ api/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     # Rotas da api com prefixo api/ (ver smartgames.urls)
     path("", views.api_index, name="api_index"),
+    path("jogos", views.api_jogos, name="api_jogos"),
 ]
--- api/views.py
+++ api/views.py
@@ -3,7 +3,9 @@
 # As rotas estão no smartgames.urls e api.urls 
 
 # from django.shortcuts import render
+from django.forms import model_to_dict
 from django.http import JsonResponse
+from .models import Jogo
 
 # Escreva as rotas aqui
 def api_index(request):
@@ -11,3 +13,15 @@ def api_index(request):
     Rota inicial para teste da api
     """
     return JsonResponse({"msg": "Olá mundo!"})
+
+def api_jogos(request):
+    """
+    Retorna uma lista de jogos
+    """
+    # Para cada jogo:
+    # 1. Transformar jogo em dictionary com model_to_dict
+    # 2. Adicionar dictionary a lista (com list comprehension)
+    # 3. Passar lista para dados
+    dados = [model_to_dict(jogo) for jogo in Jogo.objects.all()]
+    # Retornar resposta JSON com dados
+    return JsonResponse({"jogos": dados})
