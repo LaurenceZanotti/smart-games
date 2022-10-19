e9a81768e306d261e8e9ac441dda91db9e10b51c
Laurence Zanotti <lau_zanotti@hotmail.com>
2022-10-19 09:52:36 -0300

    Tratamento de métodos inválidos + Rota GET /api/jogos/id retorna jogo unico

--- api/urls.py
+++ api/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     # Rotas da api com prefixo api/ (ver smartgames.urls)
     path("", views.api_index, name="api_index"),
     path("jogos", views.api_jogos, name="api_jogos"),
+    path("jogos/<int:id_jogo>", views.api_jogo, name="api_jogo"),
 ]
--- api/views.py
+++ api/views.py
@@ -18,10 +18,35 @@ def api_jogos(request):
     """
     Retorna uma lista de jogos
     """
-    # Para cada jogo:
-    # 1. Transformar jogo em dictionary com model_to_dict
-    # 2. Adicionar dictionary a lista (com list comprehension)
-    # 3. Passar lista para dados
-    dados = [model_to_dict(jogo) for jogo in Jogo.objects.all()]
-    # Retornar resposta JSON com dados
-    return JsonResponse({"jogos": dados})
+    if request.method == "GET":
+        # Para cada jogo:
+        # 1. Transformar jogo em dictionary com model_to_dict
+        # 2. Adicionar dictionary a lista (com list comprehension)
+        # 3. Passar lista para dados
+        dados = [model_to_dict(jogo) for jogo in Jogo.objects.all()]
+        # Retornar resposta JSON com dados
+        return JsonResponse({"jogos": dados})
+    else:
+        # Retornar erro se método não for GET
+        return JsonResponse({"msg": "Método inválido"}, status=405)
+
+def api_jogo(request, id_jogo):
+    """
+    Retorna um único jogo
+    """
+    if request.method == "GET":
+        dado = None
+        try:
+            # Tentar obter jogo
+            jogo = Jogo.objects.get(id=id_jogo)
+            # Transformar model em dictionary
+            dado = model_to_dict(jogo)
+        except Jogo.DoesNotExist:
+            # Se não existir, avisar que jogo não existe
+            return JsonResponse({"msg": "Jogo não existe"}, status=404)
+
+        # Retornar jogo
+        return JsonResponse({"jogo": dado})
+    else:
+        # Retornar erro se método não for GET
+        return JsonResponse({"msg": "Método inválido"}, status=405)
